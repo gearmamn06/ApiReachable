541d6ca440268eb19061a84ab0428912222e1c6e
박현수 <sudo@amanda.co.kr>
2019-03-23 22:39:36 +0900

    First commit - SelfNameable

---
+++ ApiReachable/Sources/ApiReachable.h
@@ -0,0 +1,19 @@
+//
+//  ApiReachable.h
+//  ApiReachable
+//
+//  Created by ParkHyunsoo on 23/03/2019.
+//  Copyright © 2019 ParkHyunsoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ApiReachable.
+FOUNDATION_EXPORT double ApiReachableVersionNumber;
+
+//! Project version string for ApiReachable.
+FOUNDATION_EXPORT const unsigned char ApiReachableVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ApiReachable/PublicHeader.h>
+
+
---
+++ ApiReachable/Sources/SelfNameable.swift
@@ -0,0 +1,22 @@
+//
+//  SelfNameable.swift
+//  ApiReachable
+//
+//  Created by ParkHyunsoo on 23/03/2019.
+//  Copyright © 2019 ParkHyunsoo. All rights reserved.
+//
+
+import Foundation
+
+protocol SelfNameable { }
+
+public extension SelfNameable {
+    
+    static var name: String {
+        return String(describing: self)
+    }
+    
+    var name: String {
+        return String(describing: type(of: self))
+    }
+}
