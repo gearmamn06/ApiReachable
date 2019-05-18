e1b49b20c77b892a9f678ab6fe970076e910d8fb
박현수 <sudo@amanda.co.kr>
2019-05-18 14:13:23 +0900

    Add testCode - Mappable -> mapping itunes search result - Mappable -> reach ituens search

---
+++ ApiReachableTests/ApiReachableTests.swift
@@ -0,0 +1,160 @@
+//
+//  ApiReachableTests.swift
+//  ApiReachableTests
+//
+//  Created by ParkHyunsoo on 16/05/2019.
+//  Copyright © 2019 ParkHyunsoo. All rights reserved.
+//
+
+import XCTest
+import ObjectMapper
+@testable import ApiReachable
+
+
+struct Genre: ImmutableMappable, Equatable {
+    let id: Int
+    let name: String
+    
+    init(map: Map) throws {
+        id = try map.value("primaryGenreId")
+        name = try map.value("primaryGenreName")
+    }
+    
+    static func == (lhs: Genre, rhs: Genre) -> Bool {
+        return lhs.id == rhs.id
+            && lhs.name == rhs.name
+    }
+}
+
+struct MappableItuenseSearchResultItem: ImmutableMappable, Equatable {
+    let type: String
+    let name: String
+    let linkUrl: URL
+    let id: Int
+    let primaryGenre: Genre
+    
+    init(map: Map) throws {
+        type = try map.value("artistType")
+        name = try map.value("artistName")
+        let urlString: String = try map.value("artistLinkUrl")
+        guard let url = URL(string: urlString) else {
+            throw ApiBaseError.mappingFail(map)
+        }
+        linkUrl = url
+        id = try map.value("artistId")
+        primaryGenre = try Genre(map: map)
+    }
+    
+    static func == (lhs: MappableItuenseSearchResultItem,
+                    rhs: MappableItuenseSearchResultItem) -> Bool {
+        return lhs.id == rhs.id
+            && lhs.type == rhs.type
+            && lhs.name == rhs.name
+            && lhs.linkUrl == rhs.linkUrl
+            && lhs.primaryGenre == rhs.primaryGenre
+    }
+}
+
+
+extension Genre {
+    
+    private init() {
+        id = 21
+        name = "Rock"
+    }
+    
+    fileprivate static var rock: Genre {
+        return Genre()
+    }
+}
+
+
+extension MappableItuenseSearchResultItem {
+    
+    private init() {
+        type = "Artist"
+        name = "Jack Johnson"
+        linkUrl = URL(string: "https://itunes.apple.com/us/artist/jack-johnson/909253?uo=4")!
+        id = 909253
+        primaryGenre = Genre.rock
+    }
+    
+    
+    fileprivate static var jackJohnson: MappableItuenseSearchResultItem {
+        return MappableItuenseSearchResultItem()
+    }
+}
+
+struct MappableItuenseSearchResult: ImmutableMappable, ApiReachable {
+    
+    static var baseURL: URL = URL(string: "https://itunes.apple.com")!
+    static var endPoint: String = "lookup"
+    static var requiredParams: [String : Any] = [:]
+    
+    let resultCount: Int
+    let results: [MappableItuenseSearchResultItem]
+    
+    init(map: Map) throws {
+        resultCount = try map.value("resultCount")
+        results = try map.value("results")
+    }
+}
+
+
+class ApiReachableTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testMapping() {
+        let testBundle = Bundle(for: type(of: self))
+        let path = testBundle.path(forResource: "Jack_Johnson_search_result", ofType: "json")
+        let fileUrl = URL(fileURLWithPath: path!)
+        let jsonString = try! String(contentsOf: fileUrl)
+        
+        let mappingResult = try? MappableItuenseSearchResult(JSONString: jsonString)
+        
+        XCTAssertNotNil(mappingResult)
+        XCTAssertEqual(mappingResult?.results.count, mappingResult?.resultCount)
+        XCTAssertEqual(mappingResult?.resultCount, 1)
+        
+        let first = mappingResult?.results.first
+        let jackJson = MappableItuenseSearchResultItem.jackJohnson
+        XCTAssertNotNil(first)
+        XCTAssertEqual(first, jackJson)
+    }
+    
+    
+    func testReach() {
+        
+        let promise = expectation(description: "reach end handle invoke")
+        
+        let queries: [String: Any] = ["id": 909253]
+        var responseModel: MappableItuenseSearchResult?
+        var responseError: Error?
+        
+        // when
+        MappableItuenseSearchResult.reach(method: .get, queries: queries, completeHandler: { result in
+            switch result {
+            case .success(let model):
+                responseModel = model
+                
+            case .fail(let error):
+                responseError = error
+            }
+            promise.fulfill()
+        })
+        
+        waitForExpectations(timeout: 5, handler: nil)
+        
+        // then
+        XCTAssertNil(responseError)
+        XCTAssertNotNil(responseModel)
+    }
+
+}
--- Podfile
+++ Podfile
@@ -1,14 +1,30 @@
 # Uncomment the next line to define a global platform for your project
 # platform :ios, '9.0'
 
-target 'ApiReachable' do
-  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
-  use_frameworks!
+workspace 'ApiReachable'
+use_frameworks!
 
-  # Pods for ApiReachable
+def shared
   pod 'ObjectMapper', '~> 3.4'
   pod 'Alamofire', '~> 4.8.2'
   pod 'RxSwift',    '~> 5.0'
+end
+
+target 'ApiReachable' do
+
+  shared
+
+end
 
 
+target 'ApiReachableTests' do
+  
+  shared
+  
+end
+
+
+target 'ApiReachable-Demo' do
+  pod 'ObjectMapper', '~> 3.4'
+  project 'ApiReachable-Demo/ApiReachable-Demo'
 end
